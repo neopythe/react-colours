db153c43432ace6e212ab5bedbacc4bf3ac13b11
Evan Bay <neopythe@protonmail.ch>
2022-07-20 16:13:13 -0600

    create Palette component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
+import seedColours from './seedColours'
+
+import Palette from './components/Palette'
+
 export default class App extends Component {
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <Palette {...seedColours[4]} />
+      </div>
+    )
   }
 }
---
+++ src/components/Palette.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colours">{/* colour boxes */}</div>
+        {/* Footer goes here */}
+      </div>
+    )
+  }
+}
